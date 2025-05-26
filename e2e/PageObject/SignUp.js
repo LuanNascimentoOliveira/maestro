2bfc4d3cb2ce2efdd51b7846956f4116646598ed
Luan Nascimento Oliveira <luanoliveira1107@gmail.com>
2025-05-26 12:17:31 +0100

    Tests e2e

--- e2e/PageObject/SignUp.js
+++ e2e/PageObject/SignUp.js
@@ -1,9 +1,17 @@
 //This file is part of the e2e project.We are using this file to test the Sign Up page of the app.
 //The file contains the locators for the Sign Up page.
+
+const date = new Date();
+const day = String(date.getDate()).padStart(2, '0');
+const month = String(date.getMonth() + 1).padStart(2, '0');
+const year = date.getFullYear();
+const currentDate = `${day}/${month}/${year}`;
+
 output.SignUp = {
     firsTimeBtn: 'First time? Get started', 
     minCharacterText: "Minimum 4 characters",
     maxCharacterText: "Maximum 20 characters",
+    minCharacterPasswordText: "6 characters",
     onlyCharacterText: "Only alphanumeric characters",
     lowercaseLetterText: "One lowercase letter",
     uppercaseLetterText: "One uppercase letter",
@@ -26,4 +34,7 @@ output.SignUp = {
     trainerIntroText: "I'm João and I'll be your personal trainer.",
     emailAlreadyInUseText: "This email is already in use!",
     invalidEmailText: "Please enter a valid email address",
+    birthdateText: currentDate,
+    yearText: "android:id/date_picker_header_year",
+    messageBirthdate: "You must be at least 13 years old to create an account",
   };
